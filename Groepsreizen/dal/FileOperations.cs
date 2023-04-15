72688d1afd0c2bf2f80ab994176c240298f62f0f
ChristopheM98 <r0901658@student.thomasmore.be>
2023-04-15 12:09:12 +0200

    File- en databaseoperations

--- Groepsreizen/dal/FileOperations.cs
+++ Groepsreizen/dal/FileOperations.cs
@@ -6,7 +6,19 @@ using System.Threading.Tasks;
 
 namespace dal
 {
-    internal class FileOperations
+    public static class FileOperations
     {
+        public static void FoutLoggen(Exception fout)
+        {
+            using (StreamWriter writer = new StreamWriter("foutenbestand.txt", true))
+            {
+                writer.WriteLine(DateTime.Now.ToString("HH:mm:ss tt"));
+                writer.WriteLine(fout.GetType().Name);
+                writer.WriteLine(fout.Message);
+                writer.WriteLine(fout.StackTrace);
+                writer.WriteLine(new String('-', 80));
+                writer.WriteLine();
+            }
+        }
     }
 }
