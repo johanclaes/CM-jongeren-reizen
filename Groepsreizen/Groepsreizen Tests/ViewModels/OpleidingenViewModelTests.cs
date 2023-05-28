66af0e642fb482f537bcfd179eb8462d18aeaf0f
johanclaes <s0085147@student.thomasmore.be>
2023-05-28 07:51:39 +0200

    met 2 unit testen

---
+++ Groepsreizen/Groepsreizen Tests/ViewModels/OpleidingenViewModelTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using wpf.ViewModels;
+
+// als we op de zoek knop drukken, dan moeten er meer dan 5 records verschijnen
+
+
+namespace Groepsreizen_Tests.ViewModels
+{
+    [TestFixture]
+    public class OpleidingenViewModelTests
+    {
+        OpleidingenViewModel opleidingenViewModel = new OpleidingenViewModel();
+
+        [Test]
+        public void OpleidingtypesTellen()
+        {
+            opleidingenViewModel.ZoekDeelnemer();
+
+            Assert.That(opleidingenViewModel.GezochteDeelnemers.Count, Is.GreaterThan(5));
+        
+        }
+    }
+}
---
+++ Groepsreizen/Groepsreizen Tests/Views/OpleidingenWindowTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Groepsreizen_Tests.Views
+{
+    internal class OpleidingenWindowTests
+    {
+    }
+}
