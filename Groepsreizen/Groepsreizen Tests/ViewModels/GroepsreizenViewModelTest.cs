de7434fcc2c02e8038a481e7b334fc60a404317d
r06658604 <r0658604@student.thomasmore.be>
2023-05-29 14:12:50 +0200

    tekstvelden toegevoegd

---
+++ Groepsreizen/Groepsreizen Tests/ViewModels/GroepsreizenViewModelTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using wpf.UserControls;
+using wpf.ViewModels;
+
+namespace Groepsreizen_Tests.ViewModels
+{
+    [TestFixture]
+    public class GroepsreizenViewModelTest
+    {
+        GroepsreizenViewModel gvm = new GroepsreizenViewModel();
+
+        [Test]
+        public void ReisAanmaken()
+        {
+            
+        }
+    }
+}
